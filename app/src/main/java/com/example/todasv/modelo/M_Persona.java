0a4c597f0e005484ead6c0919b6c23193d1512ae
paulaoviedo <paulaj.oviedo@correo.usa.edu.co>
2019-03-14 02:32:17 -0500

    modelo persona

---
+++ app/src/main/java/com/example/todasv/modelo/M_Persona.java
@@ -0,0 +1,17 @@
+package com.example.todasv.modelo;
+
+public class ModeloPersona {
+    private String nombre ;
+    private String apellido;
+    private String Correo;
+    private int cc;
+    private String clave ;
+    private String  profesion ;
+    private String fecha_Expe;
+    private int tel ;
+
+
+
+    
+
+}
